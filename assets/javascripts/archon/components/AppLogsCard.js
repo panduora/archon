f7925475ab6f11dbefe86c7c90c61edcfd1f6af4
chaoyiwang <chaoyiwang@creditease.cn>
2017-07-03 16:35:40 +0800

    add oplog ui

---
+++ assets/javascripts/archon/components/AppLogsCard.js
@@ -0,0 +1,40 @@
+import React from 'react';
+
+import Dialog from './Dialog';
+import MDL from './MdlComponents.js'
+
+let AppLogsCard = React.createClass({
+
+  contextTypes:{
+    theme: React.PropTypes.object,
+  },
+
+  render(){
+    const {theme}=this.context;
+    const logs = JSON.parse(this.props.logs);
+    const appname = this.props.appname;
+    const titleStyle = _.assign({}, theme.cardTitle, theme.colorStyle('info', true));
+    //const versions=["hello","world"];
+    return (
+      <MDL.Card style={theme.card}>
+        <MDL.CardTitle style={titleStyle} title={`应用 - ${appname} 的操作历史`} />
+        <div style={{ marginBottom: 16  }}>
+          <MDL.Table style={theme.dataTable}
+            cols={[
+              { title: "操作时间", number: false },
+              { title: "操作名称", number: false },
+              { title: "操作人员", number: false },
+              { title: "版本号", number: false },
+              { title: "备注", number: false },
+            ]}
+            rows={ _.map(logs,(l) => [l.fields.time, l.fields.op, l.fields.user, l.fields.app_version, l.fields.message])}
+          />
+        </div>
+      </MDL.Card>
+    );
+  },
+
+
+})
+
+export default AppLogsCard;
---
+++ assets/javascripts/archon/pages/AppLogsPage.js
@@ -0,0 +1,64 @@
+import React from 'react';
+import {History} from 'react-router';
+
+import * as AppActions from '../models/actions/Apps'
+import MDL from '../components/MdlComponents'
+import AppLogsCard from '../components/AppLogsCard';
+import AuthorizeMixin from '../mixins/AuthorizeMixin';
+import ApiMixin from '../mixins/ApiMixin';
+import FlashMessageMixin from '../mixins/FlashMessageMixin'
+
+let AppLogsPage = React.createClass({
+
+  mixins: [History, AuthorizeMixin, FlashMessageMixin, ApiMixin],
+  
+  //contextTypes: {
+  //  theme: React.PropTypes.object,
+  //},
+
+  componentDidMount(){
+    this.authorize(()=>{
+      this.refreshLogs();
+    });
+  },
+
+  componentWillReceiveProps(nextProps){
+    const name = this.getAppName();
+    const newName = nextProps.params.appName;
+  },
+
+  refreshLogs(props = this.props){
+    const {dispatch} = props;
+    const name = this.getAppName(props);
+    if (name) {
+      dispatch(AppActions.getLogs(name));
+    }
+  },
+
+  render(){
+    const name = this.getAppName();
+    const request = this.getRequest(['GET_APPLOGS_REQUEST']);
+    const logs = request.data;
+    return (
+      <MDL.Grid>
+        <MDL.GridCell col={8}>
+          <AppLogsCard appname={name} logs={logs} />
+          <MDL.CardActions border buttons={[
+            { title: '刷新', to: `/archon/apps/${name}/logs`, color: 'colored'  },
+            { title: '返回应用', to: `/archon/apps/${name}`, color: 'colored' },
+          ]}
+            border={true} align='right'
+          />
+        </MDL.GridCell>
+      </MDL.Grid>
+    );
+  },
+  
+  getAppName(props = this.props) {
+    return props.params.appName;
+  },
+
+
+});
+
+export default AppLogsPage;
